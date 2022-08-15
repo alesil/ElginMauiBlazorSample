7140028b6ff9f04731ce896b389937dfc6b62dbf
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-15 18:55:29 -0300

    Add project files.

---
+++ ElginMauiBlazorSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ElginMauiBlazorSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
