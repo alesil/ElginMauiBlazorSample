5c2dc3964e955a08018e8a6f1a8885a2f8765529
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-16 11:30:18 -0300

    Remoção das Platforms iOS, MacCatalyst e Tizen

---
+++ ElginMauiBlazorSample/Services/E1BridgeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElginMauiBlazorSample.Services;
+public partial class E1BridgeService
+{
+	public E1BridgeService()
+	{
+		// DoConstructor();
+	}
+
+	// private partial void DoConstructor();
+}
