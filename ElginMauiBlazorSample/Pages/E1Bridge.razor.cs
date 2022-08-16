c8ccfce5353c7ec145715b768c1f2bebc491e5d8
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-16 20:08:12 -0300

    Alterado o numero de parcelas para 2

--- ElginMauiBlazorSample/Pages/E1Bridge.razor.cs
+++ ElginMauiBlazorSample/Pages/E1Bridge.razor.cs
@@ -14,7 +14,7 @@ public partial class E1Bridge : ComponentBase
     {
         public string Ip { get; set; } = "192.168.3.20";
         public decimal Valor { get; set; } = 2.50m;
-        public int Parcelas { get; set; } = 1;
+        public int Parcelas { get; set; } = 2;
         public int PortaTransacao { get; set; } = 3000;
         public int PortaStatus { get; set; } = 3001;
         public bool EnviarSenha { get; set; } = false;
