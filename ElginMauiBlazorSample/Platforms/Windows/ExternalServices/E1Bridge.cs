3c5015b680d72d33cd4ea722e3681676f50bd342
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-17 01:04:37 -0300

    Tentativa de usar a DLL Windows, com falhas ainda

---
+++ ElginMauiBlazorSample/Platforms/Windows/ExternalServices/E1Bridge.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ElginMauiBlazorSample.Platforms.Windows.ExternalServices;
+public class E1BridgeW
+{
+    public const string DLL = @"Platforms\Windows\Library\E1_Bridge01_x64.dll";
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string IniciaVenda(int idTransacao, string pdv, string valorTotal);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string IniciaVendaDebito(int idTransacao, string pdv, string valorTotal);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string IniciaVendaCredito(int idTransacao, string pdv, string valorTotal, int tipoFinanciamento, int numParcelas);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string IniciaCancelamentoVenda(int idTransacao, string pdv, string valorTotal, string dataHora, string nsu);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string IniciaOperacaoAdministrativa(int idTransacao, string pdv, int operacao);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string ImprimirCupomNfce(string xml, int indexcsc, string csc);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string ImprimirCupomSat(string xml);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string ImprimirCupomSatCancelamento(string xml, string assQRCode);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string SetSenha(string senha, bool habilitada);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string ConsultarStatus();
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern int GetTimeoutRef(ref string retorno);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string ConsultarUltimaTransacao(string pdv);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string SetSenhaServer(string senha, bool habilitada);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string SetTimeout(int timeout);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern int GetServerRef(ref string retorno);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    internal static extern string SetServer(string ipTerminal, int portaTransacao, int portaStatus);
+
+}
--- ElginMauiBlazorSample/Platforms/Windows/Services/E1BridgeService.cs
+++ ElginMauiBlazorSample/Platforms/Windows/Services/E1BridgeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ElginMauiBlazorSample.Platforms.Windows.ExternalServices;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,82 +17,85 @@ public partial class E1BridgeService
 
     public partial string IniciaVenda(int idTransacao, string pdv, string valorTotal)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.IniciaVenda(idTransacao, pdv, valorTotal);
     }
 
     public partial string IniciaVendaDebito(int idTransacao, string pdv, string valorTotal)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.IniciaVendaDebito(idTransacao, pdv, valorTotal);
     }
 
     public partial string IniciaVendaCredito(int idTransacao, string pdv, string valorTotal, int tipoFinanciamento, int numParcelas)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.IniciaVendaCredito(idTransacao, pdv, valorTotal, tipoFinanciamento, numParcelas);
     }
 
     public partial string IniciaCancelamentoVenda(int idTransacao, string pdv, string valorTotal, string dataHora, string nsu)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.IniciaCancelamentoVenda(idTransacao, pdv, valorTotal, dataHora, nsu);
     }
 
     public partial string IniciaOperacaoAdministrativa(int idTransacao, string pvd, int operacao)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.IniciaOperacaoAdministrativa(idTransacao, pvd, operacao);
     }
 
     public partial string ImprimirCupomNfce(string xml, int indexcsc, string csc)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.ImprimirCupomNfce(xml, indexcsc, csc);
     }
 
     public partial string ImprimirCupomSat(string xml)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.ImprimirCupomSat(xml);
     }
 
     public partial string ImprimirCupomSatCancelamento(string xml, string assQRCode)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.ImprimirCupomSatCancelamento(xml, assQRCode);
     }
 
     public partial string SetSenha(string senha, bool habilitada)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.SetSenha(senha, habilitada);
     }
 
     public partial string ConsultarStatus()
     {
-        throw new NotImplementedException();
+        return E1BridgeW.ConsultarStatus();
     }
 
     public partial string GetTimeout()
     {
-        throw new NotImplementedException();
+        //return E1BridgeW.Timeout;
+        return "NÃO IMPLEMENTADO";
     }
 
     public partial string ConsultarUltimaTransacao(string pdv)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.ConsultarUltimaTransacao(pdv);
     }
 
     public partial string SetSenhaServer(string senha, bool habilitada)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.SetSenhaServer(senha, habilitada);
     }
 
     public partial string SetTimeout(int timeout)
     {
-        throw new NotImplementedException();
+        return E1BridgeW.SetTimeout(timeout);
     }
 
     public partial string GetServer()
     {
-        throw new NotImplementedException();
+        // return E1BridgeW.Server;
+        return "NÃO IMPLEMENTADO";
     }
 
     public partial string SetServer(string ipTerminal, int portaTransacao, int portaStatus)
     {
-        throw new NotImplementedException();
+        //var x =  E1BridgeW.SetServer(ipTerminal, portaTransacao, portaStatus);
+        return E1BridgeW.SetServer(ipTerminal, portaTransacao, portaStatus);
     }
 }
 
