b95f6355176a56ef0f84fa8108069288b5972b63
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-17 18:34:21 -0300

    Alteração nas chamdas da DLL Windows, usando IntPtr

--- ElginMauiBlazorSample/Platforms/Windows/Services/E1BridgeService.cs
+++ ElginMauiBlazorSample/Platforms/Windows/Services/E1BridgeService.cs
@@ -10,57 +10,51 @@ public partial class E1BridgeService
     private partial void DoConstructor() { }
 
     public partial string IniciaVenda(int idTransacao, string pdv, string valorTotal)
-        => E1BridgeWin.IniciaVenda(idTransacao, pdv, valorTotal);
+        => E1BridgeWin.IniciaVenda_RetornoDireto(idTransacao, pdv, valorTotal);
 
     public partial string IniciaVendaDebito(int idTransacao, string pdv, string valorTotal)
-        => E1BridgeWin.IniciaVendaDebito(idTransacao, pdv, valorTotal);
+        => E1BridgeWin.IniciaVendaDebito_RetornoDireto(idTransacao, pdv, valorTotal);
 
     public partial string IniciaVendaCredito(int idTransacao, string pdv, string valorTotal, int tipoFinanciamento, int numParcelas)
-        => E1BridgeWin.IniciaVendaCredito(idTransacao, pdv, valorTotal, tipoFinanciamento, numParcelas);
+        => E1BridgeWin.IniciaVendaCredito_RetornoDireto(idTransacao, pdv, valorTotal, tipoFinanciamento, numParcelas);
 
     public partial string IniciaCancelamentoVenda(int idTransacao, string pdv, string valorTotal, string dataHora, string nsu)
-        => E1BridgeWin.IniciaCancelamentoVenda(idTransacao, pdv, valorTotal, dataHora, nsu);
+        => E1BridgeWin.IniciaCancelamentoVenda_RetornoDireto(idTransacao, pdv, valorTotal, dataHora, nsu);
 
     public partial string IniciaOperacaoAdministrativa(int idTransacao, string pvd, int operacao)
-        => E1BridgeWin.IniciaOperacaoAdministrativa(idTransacao, pvd, operacao);
+        => E1BridgeWin.IniciaOperacaoAdministrativa_RetornoDireto(idTransacao, pvd, operacao);
 
     public partial string ImprimirCupomNfce(string xml, int indexcsc, string csc)
-        => E1BridgeWin.ImprimirCupomNfce(xml, indexcsc, csc);
+        => E1BridgeWin.ImprimirCupomNfce_RetornoDireto(xml, indexcsc, csc);
 
     public partial string ImprimirCupomSat(string xml)
-        => E1BridgeWin.ImprimirCupomSat(xml);
+        => E1BridgeWin.ImprimirCupomSat_RetornoDireto(xml);
 
     public partial string ImprimirCupomSatCancelamento(string xml, string assQRCode)
-        => E1BridgeWin.ImprimirCupomSatCancelamento(xml, assQRCode);
+        => E1BridgeWin.ImprimirCupomSatCancelamento_RetornoDireto(xml, assQRCode);
 
     public partial string SetSenha(string senha, bool habilitada)
-        => E1BridgeWin.SetSenha(senha, habilitada);
+        => E1BridgeWin.SetSenha_RetornoDireto(senha, habilitada);
 
     public partial string ConsultarStatus()
-        => E1BridgeWin.ConsultarStatus();
+        => E1BridgeWin.ConsultarStatus_RetornoDireto();
 
     public partial string GetTimeout()
-    {
-        //return E1BridgeW.Timeout;
-        return "NÃO IMPLEMENTADO";
-    }
+        => E1BridgeWin.GetTimeout_RetornoDireto();
 
     public partial string ConsultarUltimaTransacao(string pdv)
-        => E1BridgeWin.ConsultarUltimaTransacao(pdv);
+        => E1BridgeWin.ConsultarUltimaTransacao_RetornoDireto(pdv);
 
     public partial string SetSenhaServer(string senha, bool habilitada)
-        => E1BridgeWin.SetSenhaServer(senha, habilitada);
+        => E1BridgeWin.SetSenhaServer_RetornoDireto(senha, habilitada);
 
     public partial string SetTimeout(int timeout)
-        => E1BridgeWin.SetTimeout(timeout);
+        => E1BridgeWin.SetTimeout_RetornoDireto(timeout);
 
     public partial string GetServer()
-    {
-        // return E1BridgeW.Server;
-        return "NÃO IMPLEMENTADO";
-    }
+        => E1BridgeWin.GetServer_RetornoDireto();
 
     public partial string SetServer(string ipTerminal, int portaTransacao, int portaStatus)
-        => E1BridgeWin.SetServer(ipTerminal, portaTransacao, portaStatus);
+        => E1BridgeWin.SetServer_RetornoDireto(ipTerminal, portaTransacao, portaStatus);
 }
 
