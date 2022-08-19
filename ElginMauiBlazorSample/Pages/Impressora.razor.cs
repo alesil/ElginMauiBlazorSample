df50d4b6dd733694286444016556ef0f7d3122fb
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2022-08-19 20:25:08 -0300

    Página inicial de impressão

---
+++ ElginMauiBlazorSample/Pages/Impressora.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElginMauiBlazorSample.Pages;
+public partial class Impressora : ComponentBase
+{
+    private bool _showSpinner = false;
+    private readonly Dados _dados = new();
+
+    private void ShowSpinner()
+    {
+        _showSpinner = true;
+        StateHasChanged();
+    }
+
+    private void HideSpinner()
+    {
+        _showSpinner = false;
+        StateHasChanged();
+    }
+
+
+    private class Dados
+    {
+        public ETipoImpresora TipoImpresora { get; set; } = ETipoImpresora.Interna;
+        public string ImpressoraIp { get; set; } = "192.168.2.160:9100";
+    }
+
+    private enum ETipoImpresora
+    {
+        Interna,
+        Usb,
+        Ethernet
+    }
+}
+
